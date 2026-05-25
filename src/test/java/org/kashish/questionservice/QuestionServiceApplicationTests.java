ddbd2ef73439a701937cd56652d95cabef401574
kashishsinghyadav <kashishhsinghhh@gmail.com>
2026-05-25 09:53:43 +0530

    initial commit

---
+++ src/test/java/org/kashish/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kashish.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
